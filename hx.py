66f0cad6f4c1ed23c2ced7af2c46a563978859d0
DmitryDzz <DzakhovDS@gmail.com>
2017-12-02 12:41:06 +0300

    Now hx.py can send/receive packets to/from HerkuleX servo

--- hx.py
+++ hx.py
@@ -1,7 +1,6 @@
 import argparse
-
+import serial
 import sys
-
 GOOD = "\033[92mGOOD\033[0m"
 BAD = "\033[91mBAD\033[0m"
 
@@ -24,7 +23,12 @@ def checksum2(data):
     return (~result) & 0xFE
 
 
-def print_packet(label, data, c1, c2):
+def print_packet(label, data, show_calculated_checksums):
+    c1 = checksum1(data)
+    c2 = checksum2(data)
+    if show_calculated_checksums:
+        data[5] = c1
+        data[6] = c2
     sys.stdout.write("\033[0m")
     sys.stdout.write(label)
     sys.stdout.write("\033[93m")
@@ -32,12 +36,18 @@ def print_packet(label, data, c1, c2):
         if i > 0:
             sys.stdout.write(" ")
         if i == 5:
-            sys.stdout.write("\033[95m")
-            sys.stdout.write("{:02X}".format(c1))
+            if data[5] == c1:
+                sys.stdout.write("\033[92m")
+            else:
+                sys.stdout.write("\033[91m")
+            sys.stdout.write("{:02X}".format(c1 if show_calculated_checksums else data[5]))
             sys.stdout.write("\033[93m")
         elif i == 6:
-            sys.stdout.write("\033[95m")
-            sys.stdout.write("{:02X}".format(c2))
+            if data[6] == c2:
+                sys.stdout.write("\033[92m")
+            else:
+                sys.stdout.write("\033[91m")
+            sys.stdout.write("{:02X}".format(c2 if show_calculated_checksums else data[6]))
             sys.stdout.write("\033[93m")
         else:
             sys.stdout.write("{:02X}".format(data[i]))
@@ -52,11 +62,17 @@ parser = argparse.ArgumentParser(
            "python3 hx.py \"FF FF 07 FE 07 FE 00\"" +
            "\n" +
            "If errors:\n" +
-           "sudo apt-get install python3-pil",
+           "sudo apt-get install python3-pil\n" +
+           "sudo easy_install -U pyserial",
     formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument(
     "packet",
     help="packet to check")
+parser.add_argument(
+    "-r",
+    "--run",
+    action='store_true',
+    help="send to servo and run")
 args = parser.parse_args()
 
 packet = bytearray.fromhex(args.packet)
@@ -91,9 +107,38 @@ print("Length:", length_output)
 print("Checksum1:", cs1_output)
 print("Checksum2:", cs2_output)
 
-if good_header and good_length and (not good_cs1 or not good_cs2):
+if good_header and good_length:
     print()
-    print_packet("Source packet: ", packet, packet[5], packet[6])
-    print_packet("Packet with \033[92mGOOD\033[0m checksums: ", packet, cs1, cs2)
+    print_packet("Source packet: ", packet, False)
+    if not good_cs1 or not good_cs2:
+        print_packet("Packet with \033[92mGOOD\033[0m checksums: ", packet, True)
 
 print()
+
+if not args.run:
+    exit(0)
+
+# Fix the checksums with the calculated values:
+if cs1 != packet[5]:
+    packet[5] = cs1
+if cs2 != packet[6]:
+    packet[6] = cs2
+
+with serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+) as ser:
+
+    print_packet("Sending: ", packet, True)
+    ser.write(packet)
+    received = ser.read(233)
+    if len(received) >= 7:
+        description = "Received (" + str(len(received)) + " bytes): "
+        print_packet(description, received, False)
+
+# TODO: Передавать имя порта параметром.
+# TODO: Передавать скорость передачи данных параметром.
